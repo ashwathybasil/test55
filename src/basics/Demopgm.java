5b6fd135bab8dc77d0aeea6631bcf80549a46bc7
lumin <ashwathybasil@gmail.com>
2024-10-29 10:53:45 +0530

    first commit

---
+++ src/basics/Demopgm.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class Demopgm {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("luminar");
+		System.out.print("technolab");
+		System.out.println("kakkanad");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
